36174af3b72406eea96756542f9c1afaeced6d18
kiranFullStack <kiran.fullstack@outlook.com>
2021-01-12 01:42:02 +0530

    node mysql docker compose!!!!!!!!!!!!!!!!!!!!!!!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
 const mysql = require('mysql')
-const redis = require('redis')
+const express = require('express')
+// const redis = require('redis')
 
 const app = express()
 
@@ -8,7 +8,7 @@ const port = 3000
 
 // First you need to create a connection to the db
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'mysql',
   user: 'root',
   password: 'password',
 })
@@ -21,19 +21,19 @@ connection.connect((err) => {
   console.log('Connection established')
 })
 
-const client = redis.createClient()
-
-client.on('error', function (error) {
-  console.error(error)
-})
-
-client.set('key', 'value2', redis.print)
-client.get('key', redis.print)
-
 connection.end((err) => {})
 
+// const client = redis.createClient()
+
+// client.on('error', function (error) {
+//   console.error(error)
+// })
+
+// client.set('key', 'value2', redis.print)
+// client.get('key', redis.print)
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!...')
 })
 
 app.listen(port, () => {
